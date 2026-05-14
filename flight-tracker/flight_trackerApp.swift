fe5f02655fc3966818ab0a41bb7688c27668fa6a
heralk21 <heralkumar212@gmail.com>
2026-05-14 19:04:05 -0400

    Initial Commit

---
+++ flight-tracker/flight_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  flight_trackerApp.swift
+//  flight-tracker
+//
+//  Created by Heral Kumar on 2026-05-14.
+//
+
+import SwiftUI
+
+@main
+struct flight_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
